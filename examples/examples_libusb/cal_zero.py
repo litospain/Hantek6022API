ebf4494dcc92260def762d08d1afd6d051756bf4
Martin <Ho-Ro@users.noreply.github.com>
2019-10-25 18:23:25 +0200

    new zero calibration with sub-sample resolution
    Signed-off-by: Martin <Ho-Ro@users.noreply.github.com>

--- examples/examples_libusb/cal_zero.py
+++ examples/examples_libusb/cal_zero.py
@@ -40,8 +40,10 @@ def read_avg( voltage_range, sample_rate=110, repeat = 1, samples = 12 * 1024 ):
 			sum2 += sample
 			count2 += 1
 
-	avg1 = int( 0.5 + sum1 / count1 )
-	avg2 = int( 0.5 + sum2 / count2 )
+#	avg1 = int( 0.5 + sum1 / count1 )
+#	avg2 = int( 0.5 + sum2 / count2 )
+	avg1 = sum1 / count1
+	avg2 = sum2 / count2
 
 	return ( avg1, avg2 )
 
@@ -74,30 +76,58 @@ input( "Apply 0 V to both channels and press <ENTER> " )
 
 offset1 = {}
 offset2 = {}
+offset_1 = {}
+offset_2 = {}
 offhi1 = {}
 offhi2 = {}
+offhi_1 = {}
+offhi_2 = {}
+
+
 
 for gain in gainSteps:
 	# average 10 times over 100 ms (cancel 50 Hz / 60 Hz)
 	print( "Measure offset at low speed for gain ", gain )
-	raw1, raw2 = read_avg( gain, 110, 10 )
-	offset1[ gain ] = 0x80 - raw1
-	offset2[ gain ] = 0x80 - raw2
+	fine1, fine2 = read_avg( gain, 110, 10 )
+	#print( fine1, fine2 )
+	raw1 = int( fine1 + 0.5 )
+	fine1 = int( 128.5 + (fine1 - raw1) * 250.0 )
+	raw2 = int( fine2 + 0.5 )
+	fine2 = int( 128.5 + (fine2 - raw2) * 250.0 )
+	#print( raw1, fine1, raw2, fine2 )
+	offset1[ gain ] = raw1
+	offset_1[ gain ] = fine1
+	offset2[ gain ] = raw2
+	offset_2[ gain ] = fine2
 	print( "Measure offset at high speed for gain ", gain )
-	raw1, raw2 = read_avg( gain, 30, 10 )
-	offhi1[ gain ] = 0x80 - raw1
-	offhi2[ gain ] = 0x80 - raw2
+	fine1, fine2 = read_avg( gain, 30, 10 )
+	raw1 = int( fine1 + 0.5 )
+	fine1 = int( 128.5 + (fine1 - raw1) * 250.0 )
+	raw2 = int( fine2 + 0.5 )
+	fine2 = int( 128.5 + (fine2 - raw2) * 250.0 )
+	#print( raw1, fine1, raw2, fine2 )
+	offhi1[ gain ] = raw1
+	offhi_1[ gain ] = fine1
+	offhi2[ gain ] = raw2
+	offhi_2[ gain ] = fine2
 
-ee_offset = bytearray( 32 )
+ee_offset = bytearray( scope.get_calibration_values( 32 + 16 + 32 ) )
 
 for index, gainID in enumerate( gains ):
+	# print( gains[index], offset1[gainID], offset2[gainID], offhi1[gainID], offhi2[gainID],  )
 	# prepare eeprom content
-	ee_offset[ 2 * index ] = 0x80 - offset1[ gainID ]
-	ee_offset[ 2 * index + 1 ] = 0x80 - offset2[ gainID ]
-	ee_offset[ 2 * index + 16 ] = 0x80 - offhi1[ gainID ]
-	ee_offset[ 2 * index + 17 ] = 0x80 - offhi2[ gainID ]
+	ee_offset[ 2 * index ] = offset1[ gainID ] 		# CH1 offset integer part
+	ee_offset[ 2 * index + 48 ] = offset_1[ gainID ]	# CH1 offset fractional part
+	ee_offset[ 2 * index + 1 ] = offset2[ gainID ]		# CH2 offset integer part
+	ee_offset[ 2 * index + 49 ] = offset_2[ gainID ]	# CH2 offset fractional part
+	ee_offset[ 2 * index + 16 ] = offhi1[ gainID ]		# same for CH2
+	ee_offset[ 2 * index + 64 ] = offhi_1[ gainID ]		#
+	ee_offset[ 2 * index + 17 ] = offhi2[ gainID ]		#
+	ee_offset[ 2 * index + 65 ] = offhi_2[ gainID ]		#
 
-print( "eeprom content [ 8 .. 39 ]: ", binascii.hexlify(ee_offset) )
+print( "eeprom content [  8 .. 40 ]: ", binascii.hexlify( ee_offset[  0:32 ] ) )
+print( "eeprom content [ 40 .. 56 ]: ", binascii.hexlify( ee_offset[ 32:48 ] ) )
+print( "eeprom content [ 56 .. 88 ]: ", binascii.hexlify( ee_offset[ 48:80 ] ) )
 
 
 scope.set_calibration_values( ee_offset )
