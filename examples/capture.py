1cfb10a1454c83acc653adfb387b8b96b13a8232
Martin <Ho-Ro@users.noreply.github.com>
2020-03-21 22:44:41 +0100

    update example 'capture.py' again
    Signed-off-by: Martin <Ho-Ro@users.noreply.github.com>

--- examples/capture.py
+++ examples/capture.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''
-Samples CH1 and Ch2 for a defined time and writes the voltage values to two files
+Samples CH1 and Ch2 for a defined time and writes the time stamp and the voltage values to a file
 '''
 from PyHT6022.LibUsbScope import Oscilloscope
 import time
@@ -9,8 +9,8 @@ import time
 ############
 # settings #
 ############
-sample_time    = 60     # seconds
-sample_rate    = 20e3    # slowest possible value is 20e3
+sample_time    = 60      # seconds
+sample_rate    = 20e3    # slowest possible value is 20e3 (20k, 50k, 100k, ... , 10M
 channel1_gain  = 1       # 1, 2, 5, 10
 channel2_gain  = 1       # 1, 2, 5, 10
 ############
@@ -20,60 +20,66 @@ channel2_gain  = 1       # 1, 2, 5, 10
 scope = Oscilloscope()
 scope.setup()
 scope.open_handle()
-scope.flash_firmware()
+
+# upload firmware into device's RAM
+if (not scope.is_device_firmware_present):
+    scope.flash_firmware()
+
+# read calibration values from EEPROM
 calibration = scope.get_calibration_values()
 
-alternative    = 0       # 0 = BULK, >0 = ISO, 1=3072,2=2048,3=1024 bytes per 125 us
-scope.set_interface( alternative )
+# set interface: 0 = BULK, >0 = ISO, 1=3072,2=2048,3=1024 bytes per 125 us
+scope.set_interface( 0 )
 #print("ISO" if scope.is_iso else "BULK", "packet size =", scope.packetsize)
-scope.set_num_channels( 2 ) # 1 = CH1 only, 2 = CH1 and CH2
 
+# set number of channels: 1 = CH1 only, 2 = CH1 and CH2
+scope.set_num_channels( 2 )
+
+# calculate and set the sample rate ID from real sample rate value
 if sample_rate < 1e6:
     sample_id = int( 100 + sample_rate / 10e3 )
 else:
     sample_id = int( sample_rate / 1e6 )
 scope.set_sample_rate( sample_id )
 
+# set the gain for CH1 and CH2
 scope.set_ch1_voltage_range( channel1_gain )
 scope.set_ch2_voltage_range( channel2_gain )
 
 
-def extend_callback( ch1_data, ch2_data ):
-    global data1_extend
-    global data2_extend
-    global skip1st
+# this callback is called every time when data packets arrive
+# scale the data packets and write them into the file
+def packet_callback( ch1_data, ch2_data ):
+    global skip1st, timestep, tick
     if skip1st: # skip the 1st block
         skip1st = False
         return
     ch1_scaled = scope.scale_read_data( ch1_data, channel1_gain, channel=1 )
-    capture1.write(str(ch1_scaled)[1:-1].replace(', ',chr(10)))
-    if len(ch1_data):
-        capture1.write('\n')
     ch2_scaled = scope.scale_read_data( ch2_data, channel2_gain, channel=2 )
-    capture2.write(str(ch2_scaled)[1:-1].replace(', ',chr(10)))
-    if len(ch2_data):
-        capture2.write('\n')
+    for ch1_value, ch2_value in zip( ch1_scaled, ch2_scaled ): # merge CH1 & CH2
+        capture.write( "{:<10.4g} {:< 10.4g} {:< 10.4g}\n".format( timestep, ch1_value, ch2_value ) )
+        timestep = timestep + tick
 
 
-capture1 = open('capture1_{}s.out'.format( sample_time ),'wt')
-capture2 = open('capture2_{}s.out'.format( sample_time ),'wt')
+# create file for time stamp and sample values of both channels
+capture = open('capture_{}s.out'.format( sample_time ),'wt')
 
-skip1st = True
-start_time = time.time() + scope.packetsize / sample_rate # skip 1st packet
-# print("Clearing FIFO and starting data transfer")
+skip1st = True # marker for skip of 1st (unstable) packet
+tick = 1 / sample_rate
+timestep = 0
+start_time = time.time() + scope.packetsize / sample_rate # add time for 1st skipped packet
 scope.start_capture()
-shutdown_event = scope.read_async(extend_callback, scope.packetsize, outstanding_transfers=10, raw=True)
+shutdown_event = scope.read_async( packet_callback, scope.packetsize, outstanding_transfers=10, raw=True)
 print("Capture", sample_time, "seconds ..." )
+
+# sample until time is over
 while time.time() - start_time < sample_time:
     scope.poll()
 scope.stop_capture()
-print("Stop capturing")
 shutdown_event.set()
-# print("Snooze 1")
-time.sleep(1)
-# print("Closing handle")
-scope.close_handle()
-# print("Handle closed.")
 
-capture1.close()
-capture2.close()
+# fetch remaining packets before closing the scope
+time.sleep(1)
+scope.close_handle()
+
+capture.close()
